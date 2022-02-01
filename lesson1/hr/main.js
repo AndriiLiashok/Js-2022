f778ee708a685f5d0789fd07dbace87c6be3fc47
AndriiLiashok <liashokandrii2018@gmail.com>
2022-02-01 18:08:12 +0200

    Initial commit

---
+++ lesson1/hr/main.js
@@ -0,0 +1,168 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+/*let txt = "hello";
+console.log(txt);
+alert(txt);
+document.write(`<diw> ${txt} </diw>`);
+
+let owu = "owu";
+console.log(owu);
+alert(owu);
+document.write(`<diw> ${owu} </diw>`);
+
+let com = "com";
+console.log(com);
+alert(com);
+document.write(`<diw> ${com} </diw>`);
+
+let ua = "ua";
+console.log(ua);
+alert(ua);
+document.write(`<diw> ${ua} </diw>`);
+
+let number1 = 1;
+console.log(number1);
+alert(number1);
+document.write(`${number1}`);
+
+let number2 = 10;
+console.log(number2);
+alert(number2);
+document.write(`${number2}`);
+
+const pi = 3.14;
+console.log(pi);
+alert(pi);
+document.write(`${pi}`);
+
+let number3 = -999;
+console.log(number3);
+alert(number3);
+document.write(`${number3}`);
+
+let number4 = 16;
+console.log(number4);
+alert(number4);
+document.write(`${number4}`);
+
+let booleanY= true;
+console.log(booleanY);
+alert(booleanY);
+document.write(`${booleanY}`);
+
+let booleanN= false;
+console.log(booleanN);
+alert(booleanN);
+document.write(`${booleanN}`);*/
+
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+/*let txt = "world";
+console.log(txt);
+alert(txt);
+document.write(`<diw> ${txt} </diw>`);
+
+let owu = "uwe";
+console.log(owu);
+alert(owu);
+document.write(`<diw> ${owu} </diw>`);
+
+let com = "ua";
+console.log(com);
+alert(com);
+document.write(`<diw> ${com} </diw>`);
+
+let ua = 380;
+console.log(ua);
+alert(ua);
+document.write(`<diw> ${ua} </diw>`);
+
+let number1 = 58948;
+console.log(number1);
+alert(number1);
+document.write(`${number1}`);
+
+let number2 = '10';
+console.log(number2);
+alert(number2);
+document.write(`${number2}`);
+
+let number3 = "-999";
+console.log(number3);
+alert(number3);
+document.write(`${number3}`);
+
+let number4 = 'Andrii';
+console.log(number4);
+alert(number4);
+document.write(`${number4}`);
+
+let booleanY= false;
+console.log(booleanY);
+alert(booleanY);
+document.write(`${booleanY}`);
+
+let booleanN= true;
+console.log(booleanN);
+alert(booleanN);
+document.write(`${booleanN}`);*/
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+/*
+let lastName = 'Liashok';
+let firstName = 'Andrii';
+let middleName = 'Vitaliuovych';
+let person = firstName + ' ' + lastName + ' ' + middleName;
+console.log(person);
+*/
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+/*let lastName = prompt('lastName');
+let firstName = prompt('firstName');
+let age = prompt('age');
+console.log(`Вітаю ${firstName} ${lastName}. Тобі ${age} років`)*/
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+/*let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));*/
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+//     5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+/*
+console.log(5 >= 6);
+console.log(5 === 6);
+console.log(5 > 6);
+console.log(10 <= 10);
+console.log(10 ===  10);
+console.log(10 !==  10);
+console.log(10 <  10);
+console.log(10 >  10);
+console.log(123 === '123');
+console.log(123 == '123');
+*/
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
